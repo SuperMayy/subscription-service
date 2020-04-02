30ada785e7dd770fa788a53b935d964dd47ca071
SuperMayy <mayowashowumi@outlook.com>
2020-04-02 14:27:52 +0100

    Finished

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set('view engine', 'handlebars');
 
 //Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.urlencoded({extended:false}));
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 // Set Static Folder
 app.use(express.static(`${__dirname}/public`));
@@ -21,6 +22,22 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
+//Charge Route
+app.post('/charge', urlencodedParser, (req, res) => {
+    const amount = req.body.donationAmount*100;
+    
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+      })
+      .then(customer => stripe.charges.create({
+        amount,
+        description: 'Website donation to Fionita project',
+        currency: 'gbp',
+        customer: customer.id
+      }))
+      .then(charge => res.render('success'));
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
